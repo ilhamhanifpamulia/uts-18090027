bcc5264543e6de711c20ef50c19f0ed9a856748f
ilhmhnifpmulia <john.ilham58@gmail.com>
2021-04-21 14:51:36 +0700

    18090027

--- resources/views/pegawai.blade.php
+++ resources/views/pegawai.blade.php
@@ -10,7 +10,7 @@
         <div class="container">
             <div class="card mt-5">
                 <div class="card-header bg-danger text-center">
-                Aplikasi perekaman data HP - <a href="https://github.com/ilhamhanifpamulia" target="_blank">By 18090027</a>
+                Aplikasi perekaman data HP
                 </div>
                 <div class="card-body">
                     <a href="/hp/tambah" class="btn btn-primary">Tambah Data Baru</a> <td><a href="/" class="btn btn-danger">Kembali</a></td>
@@ -26,6 +26,7 @@
                             </tr>
                         </thead>
                         <tbody>
+                        <a href="https://github.com/ilhamhanifpamulia" target="_blank">By 18090027</a>
                             @foreach($pegawai as $p)
                             <tr>
                                 <td>{{ $p->nama }}</td>
