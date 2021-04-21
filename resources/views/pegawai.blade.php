ee949fa631871a2ca6d9956eb1a012d32a04af07
ilhmhnifpmulia <john.ilham58@gmail.com>
2021-04-21 09:57:27 +0700

    18090027

--- resources/views/pegawai.blade.php
+++ resources/views/pegawai.blade.php
@@ -26,7 +26,7 @@
                             </tr>
                         </thead>
                         <tbody>
-                            @foreach($hp as $p)
+                            @foreach($pegawai as $p)
                             <tr>
                                 <td>{{ $p->nama }}</td>
                                 <td>{{ $p->alamat }}</td>
